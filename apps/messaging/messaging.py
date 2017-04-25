a79f3f811443b94f16d8c87b9edfa407461fffdc
Talha Muhammad <muhammadtalhas@yahoo.com>
2017-04-25 01:57:10 -0400

    Main Menu in text app initial push

--- apps/messaging/messaging.py
+++ apps/messaging/messaging.py
@@ -1,12 +1,67 @@
+import json
 import topBar, serialConn, pygame
 from pygame.locals import *
 
 class app():
     def __init__(self, OS, FONA):
+    #def __init__(self):
         self.OS = OS
         self.FONA=FONA
+        self.msgObjs = []
+
+        self.font=pygame.font.Font('Roboto-Regular.ttf', 40)
+        self.fontBold=pygame.font.Font('Roboto-Bold.ttf', 40)
+
+        self.currentScroll=0
+
+        self.openedConvo=""
 
     def main(self):
         self.OS.screen.fill(self.OS.WHITE)
         while not done:
             self.topBar.tick()
+            self.OS.OSUpdate(self.FONA)
+            if self.openedConvo != "":
+                self.drawMain(self.currentScroll)
+
+
+
+    def drawMain(self,firstIndex):
+        #scroll up here
+        #pos[0] 0 - 320
+        #pos[1] 25-50
+        pygame.draw.line(self.OS.screen,self.OS.BLACK,(0,50),(320,50),4)
+        #pos[0] 0 - 320
+        #pos[1] 50-252.5
+        phoneNumberOne = self.fontBold.render(self.msgObjs[firstIndex].phone_number, 1, self.BLACK)
+        self.OS.screen.blit(phoneNumberOne, (160, 151.25))
+
+        pygame.draw.line(self.OS.screen,self.OS.BLACK,(0,252.5),(320,252.5),1)
+        #pos[0] 0 - 320
+        #pos[1] 252.5-455
+        if len(msgObjs) > 1 and len(msgObjs) != firstIndex+1:
+            phoneNumberTwo = self.fontBold.render(self.msgObjs[firstIndex+1].phone_number, 1, self.BLACK)
+            self.OS.screen.blit(phoneNumberTwo, (160, 353.75))
+        pygame.draw.line(self.OS.screen,self.OS.BLACK,(0,455),(320,455),4)
+        #scroll down here
+        #pos[0] 0 - 320
+        #pos[1] 455-480
+
+
+    def loadDB(self):
+        with open('messageDB.json') as data_file:
+            data = json.load(data_file)
+            for convo in data["messages"]:
+                self.msgObjs.append(convo)
+
+if __name__ == "__main__":
+    app = app()
+    print(app.msgObjs)
+    app.loadDB()
+    print(app.msgObjs)
+
+#note
+#AT+CMGF=1 enters text mode
+#AT+CMGR=X gets texts from slots
+
+#click between
--- src/apps.py
+++ src/apps.py
@@ -24,39 +24,61 @@ class systemApps:
                     return 1
                 if event.pos[1] > 260 and event.pos[1] < 340:
                     print(5)
+                    if len(self.loadedAppObjects)>4:
+                        self.loadedAppObjects[4].main()
                     return 5
                 if event.pos[1] > 380 and event.pos[1] < 460:
                     print(9)
+                    if len(self.loadedAppObjects)>8:
+                        self.loadedAppObjects[8].main()
                     return 9
             if event.pos[0] > 80 and event.pos[0] < 160:
                 if event.pos[1] > 140 and event.pos[1] < 220:
                     print(2)
+                    if len(self.loadedAppObjects)>1:
+                        self.loadedAppObjects[1].main()
                     return 2
                 if event.pos[1] > 260 and event.pos[1] < 340:
                     print(6)
+                    if len(self.loadedAppObjects)>5:
+                        self.loadedAppObjects[5].main()
                     return 6
                 if event.pos[1] > 380 and event.pos[1] < 460:
                     print(10)
+                    if len(self.loadedAppObjects)>9:
+                        self.loadedAppObjects[9].main()
                     return 10
             if event.pos[0] > 160 and event.pos[0] < 240:
                 if event.pos[1] > 140 and event.pos[1] < 220:
                     print(3)
+                    if len(self.loadedAppObjects)>2:
+                        self.loadedAppObjects[2].main()
                     return 3
                 if event.pos[1] > 260 and event.pos[1] < 340:
                     print(7)
+                    if len(self.loadedAppObjects)>6:
+                        self.loadedAppObjects[6].main()
                     return 7
                 if event.pos[1] > 380 and event.pos[1] < 460:
                     print(11)
+                    if len(self.loadedAppObjects)>10:
+                        self.loadedAppObjects[10].main()
                     return 11
             if event.pos[0] > 240 and event.pos[0] < 320:
                 if event.pos[1] > 140 and event.pos[1] < 220:
                     print(4)
+                    if len(self.loadedAppObjects)>3:
+                        self.loadedAppObjects[3].main()
                     return 4
                 if event.pos[1] > 260 and event.pos[1] < 340:
                     print(8)
+                    if len(self.loadedAppObjects)>7:
+                        self.loadedAppObjects[7].main()
                     return 8
                 if event.pos[1] > 380 and event.pos[1] < 460:
                     print(12)
+                    if len(self.loadedAppObjects)>11:
+                        self.loadedAppObjects[11].main()
                     return 12
 
     def getAppOrder(self):
